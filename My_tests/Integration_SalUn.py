3ef29f3989a28f61ec49f8320866a4beac3b36f1
carolina-kelsch-isi <carolina.kelsch@senairs.org.br>
2025-03-11 20:46:35 -0300

    test for dataset split

---
+++ My_tests/Integration_SalUn.py
@@ -0,0 +1,171 @@
+#import copy
+import os
+from collections import OrderedDict
+import sys
+#import arg_parser
+import torch
+import torch.nn as nn
+import torch.optim
+import torch.utils.data
+#import unlearn
+from dataset_split import *
+
+def setup_seed(seed):
+    print("setup random seed = {}".format(seed))
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.backends.cudnn.deterministic = True
+
+def save_gradient_ratio(data_loaders, model, criterion, **kwargs):
+    optimizer = torch.optim.SGD(
+        model.parameters(),
+        kwargs['unlearn_lr'],
+        momentum=kwargs['momentum'],
+        weight_decay=kwargs['weight_decay'],
+    )
+
+    gradients = {}
+
+    forget_loader = data_loaders["forget"]
+    model.eval()
+
+    for name, param in model.named_parameters():
+        gradients[name] = 0
+
+    for i, (image, target) in enumerate(forget_loader):
+        image = image.cuda()
+        target = target.cuda()
+
+        # compute output
+        output_clean = model(image)
+        loss = - criterion(output_clean, target)
+
+        optimizer.zero_grad()
+        loss.backward()
+
+        with torch.no_grad():
+            for name, param in model.named_parameters():
+                if param.grad is not None:
+                    gradients[name] += param.grad.data
+
+    with torch.no_grad():
+        for name in gradients:
+            gradients[name] = torch.abs_(gradients[name])
+
+    threshold_list = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+
+    for i in threshold_list:
+        sorted_dict_positions = {}
+        hard_dict = {}
+
+        # Concatenate all tensors into a single tensor
+        all_elements = - torch.cat([tensor.flatten() for tensor in gradients.values()])
+
+        # Calculate the threshold index for the top 10% elements
+        threshold_index = int(len(all_elements) * i)
+
+        # Calculate positions of all elements
+        positions = torch.argsort(all_elements)
+        ranks = torch.argsort(positions)
+
+        start_index = 0
+        for key, tensor in gradients.items():
+            num_elements = tensor.numel()
+            # tensor_positions = positions[start_index: start_index + num_elements]
+            tensor_ranks = ranks[start_index : start_index + num_elements]
+
+            sorted_positions = tensor_ranks.reshape(tensor.shape)
+            sorted_dict_positions[key] = sorted_positions
+
+            # Set the corresponding elements to 1
+            threshold_tensor = torch.zeros_like(tensor_ranks)
+            threshold_tensor[tensor_ranks < threshold_index] = 1
+            threshold_tensor = threshold_tensor.reshape(tensor.shape)
+            hard_dict[key] = threshold_tensor
+            start_index += num_elements
+
+        torch.save(hard_dict, os.path.join(kwargs['save_dir'], "with_{}.pt".format(i)))
+
+
+def generate_fmask(model, batch_size: int, **kwargs):
+    #args = arg_parser.parse_args()
+    print('entered function...')
+
+    if torch.cuda.is_available():
+        torch.cuda.set_device(int(0))
+        device = torch.device(f"cuda:{int(0)}")
+    else:
+        device = torch.device("cpu")
+
+    os.makedirs(kwargs['save_dir'], exist_ok=True)
+
+    model.cuda()
+    
+    '''
+    if args.seed:
+        setup_seed(args.seed)
+        seed = args.seed
+    else:
+    '''
+    setup_seed(1)
+
+    current_path = os.getcwd()
+    root_dataset = current_path + '/datasets/' ## TODO: should not be hardcoded
+    ds = UnlearnDatasetSplit(root_dataset, "cifar10")
+
+    print('loaded dataset...')
+
+    splitted = ds.load_dataset()
+
+    sets = np.array(['Train','Test','Train_retain','Test_retain','Train_forget','Test_forget'])
+    if np.isin(sets, splitted.keys()).sum() != 6:
+        print(f"\033[31mNeeded datasets are not available\r\n Please pass the dictionary with the following datasets{sets}!\033[0m")
+        return None
+    
+    print('found all dataset splits...')
+    
+    retain_loader = ds.get_loader('Train_retain',
+        batchsize=batch_size,
+        num_workers=0,
+        pin_memory=True,
+        shuffle=True,
+    )
+    forget_loader = ds.get_loader('Train_forget',
+        batchsize=batch_size,
+        num_workers=0,
+        pin_memory=True,
+        shuffle=True,
+    )
+    
+    print('created loaders...')
+
+    assert len(forget_dataset) + len(retain_dataset) == len(datasets_dict['Train']), print("Sizes don't match")
+
+    print(f"number of retain dataset {len(retain_dataset)}")
+    print(f"number of forget dataset {len(forget_dataset)}")
+
+    # Validation loader is only 10 percent of the train dataset with no further
+    # changes according to the forget or retain set, and is not used here
+
+    unlearn_data_loaders = OrderedDict(forget=forget_loader)
+
+    criterion = nn.CrossEntropyLoss()
+    '''
+    if args.resume:
+        checkpoint = unlearn.load_unlearn_checkpoint(model, device, args)
+    
+    if args.resume and checkpoint is not None:
+        model, evaluation_result = checkpoint
+    
+    else:
+    '''
+    checkpoint = torch.load(kwargs['model_path'], map_location=device)
+    if "state_dict" in checkpoint.keys():
+        checkpoint = checkpoint["state_dict"]
+        model.load_state_dict(checkpoint, strict=False)
+    
+    print('running unlearning...')
+
+    save_gradient_ratio(unlearn_data_loaders, model, criterion, kwargs)
--- My_tests/dataset_split.py
+++ My_tests/dataset_split.py
@@ -1,130 +1,173 @@
-import numpy as np 
-import matplotlib as plt
+#import numpy as np 
+#import matplotlib as plt
 import torch
+from torchvision import datasets, transforms
+from torch.utils.data import Subset
+import os
+import numpy as np
+import pickle
 
+class UnlearnDatasetSplit:
 
-class UnlearnDatasetSplit():
+    available_datasets = ["cifar-10", "coco"]
+    possible_splits = ["Train", "Test", "Train_retain", "Train_forget", "Test_retain", "Test_forget"]
 
-    methods = ["UnlearnAccuracy", "RemainingAccuracy", "TestingAccuracy", "CLIP-Score", "FID", "RunTimeEfficiency"]
+    def __init__(self, path: str, dataset: str):
+        self.dataset_path = path
+        self.dataset_splits = {}
+        self.classes = None
+        self.n_classes = 0
 
-    def __init__(self, model, data_loders, method=None):
-        self.model = model
-        self.data_loders = data_loders
-        self.stats = {}
-        if method != None:
-            compute_acc(method)
+        # load from path
+        self.load_dataset()
 
-    def get_acc():
-        return self.stats
-    
-    '''From SalUn'''
-    def accuracy(output, target, topk=(1,)):
-        """Computes the precision@k for the specified values of k"""
-        maxk = max(topk)
-        batch_size = target.size(0)
-
-        _, pred = output.topk(maxk, 1, True, True)
-        pred = pred.t()
-        correct = pred.eq(target.view(1, -1).expand_as(pred))
-
-        res = []
-        for k in topk:
-            correct_k = correct[:k].view(-1).float().sum(0)
-            res.append(correct_k.mul_(100.0 / batch_size))
-        return res
-    
-    '''From SalUn'''
-    def MIA(metric):
-        shadow_train = torch.utils.data.Subset(self.data_loader["retain"], list(range(len(self.data_loader["test"]))))
-        shadow_train_loader = torch.utils.data.DataLoader(
-            shadow_train, batch_size=args.batch_size, shuffle=False
-        )
-
-        self.stats = {metric: SVC_MIA(
-            shadow_train=shadow_train_loader,
-            shadow_test=self.data_loader["test"],
-            target_train=None,
-            target_test=self.data_loader["forget"],
-            model=model,
-            )
-        }
-
-    def compute_acc(metric, loader):
-        losses = np.zeros_like(len(loader))
-        top1 = np.zeros_like(len(loader))
-
-        # switch to evaluate mode
-        self.model.eval()
-        device = (torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu"))
-
-        for i, (image, target) in enumerate(loader):
-
-            image = image.cuda()
-            target = target.cuda()
-
-            with torch.no_grad():
-                output = model(image)
-                loss = criterion(output, target)
-
-            output = output.float()
-            loss = loss.float()
-
-            prec1 = accuracy(output.data, target)[0]
-            losses[i] = loss.item()
-            top1[i] = prec1.item()
-
-            if i % args.print_freq == 0:
-                print(f"Test: [{i}/{len(loader)}]\t" + 
-                f"Loss {loss[i]:.4f} ({np.mean(loss):.4f})\t" +
-                f"Accuracy {top1[i]:.3f} ({np.mean(top1):.3f})")
-
-            print(f"Accuracy {np.mean(top1):.3f}")
-
-        self.stats = {metric: np.mean(top1)}
-
-    def compute_metric(method):
-
-        if not isinstance(method, list):
-            if not isinstance(method, str):
-                print(f"\033[31mInvalid method\r\n Please pick between {methods}!\033[0m")
-        
-        for m in method:
-            if m == "UnlearnAccuracy":
-                if "forget" in self.data_loders and self.data_loaders["forget"] is not None:
-                    compute_acc(m, self.data_loders["forget"])
-
-            elif m == "RemainingAccuracy":
-                if "retain" in self.data_loders and self.data_loaders["retain"] is not None:
-                    compute_acc(m, self.data_loders["retain"])
-
-            elif m == "TestingAccuracy":
-                if "test" in self.data_loders and self.data_loaders["test"] is not None:
-                    compute_acc(m, self.data_loders["test"])
-
-            elif m == "MIA":
-                if "forget" in self.data_loders and self.data_loaders["forget"] is not None:
-                    MIA("MIA")
-
-            elif m == "RunTimeEfficiency":
-                # TODO: check how to implement run time efficiency, probably in the implementation of the unlearning method
-                return None
+        # if could not find datasets in path
+        if len(self.dataset_splits) == 0:
+            if dataset != None:
+                self.get_dataset(dataset)
+                print(f'Classes: {self.classes}')
+                self.n_classes = len(self.classes)
             
-            elif m == "CLIP-Score":
-                '''Only makes sense for generation models: measures the accuracy of a text describing an image 
-                or an image generated from a text description'''
 
-                #CLIPScore
+    def save_dataset(self):
 
-                return None
-
-            elif m == "FID":
-                '''Only makes sense for generation models: measures the distance of two distributions,
-                in this case the distance between two images distributions'''
-                return None
-
-            else:
-                print(f"\033[31mInvalid method\r\n Please pick between {methods}!\033[0m")
-                return None
-    
-        return self.stats
+        for k in self.dataset_splits.keys():
+            with open(f'{self.dataset_path}{k}.pkl', 'wb') as f:
+                pickle.dump(self.dataset_splits[k], f)
         
+    def load_dataset(self):
+
+        if len(self.dataset_splits) == 0:
+            files = [f for f in os.listdir(self.dataset_path) if f.endswith('.pkl')]
+            print(files)
+            try:
+                for file in files:
+                    with open(self.dataset_path + file, 'rb') as f:
+                        self.dataset_splits[file[:-4]] = pickle.load(f)
+            except:
+                print("\033[31mError while loading the dataset from the files!\033[0m")
+                return None
+        
+        return self.dataset_splits
+
+    def get_dataset(self, dataset: str):
+
+        if isinstance(dataset, list) or not isinstance(dataset, str):
+            print(f"\033[31mInvalid dataset\r\n Please pick between {available_datasets}!\033[0m")
+            return None
+        
+        if dataset == "cifar10":
+
+            transform = transforms.Compose([transforms.ToTensor(),
+                                            transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))])
+            train = datasets.CIFAR10(self.dataset_path, train=True, transform=transform, download=True)
+            test = datasets.CIFAR10(self.dataset_path, train=False, transform=transform, download=True)
+
+            self.classes = train.classes
+            self.dataset_splits = {"Train": train, "Test": test}
+
+        elif dataset == "coco":
+            print('not implemented yet')
+
+        else:
+            print(f"\033[31mInvalid method\r\n Please pick between {available_datasets}!\033[0m")
+    
+    def get_loader(self, k, batchsize: int, shuffle: bool = True, num_workers: int = 0, pin_memory: bool = True):
+
+        try:
+            loader = torch.utils.data.DataLoader(self.dataset_splits[k], batch_size=batchsize, shuffle=shuffle, num_workers=num_workers, pin_memory=pin_memory)
+        except:
+            print(f"\033[31mCould not load {k}, check if dataset exists!\033[0m")
+            return None
+        return loader
+
+    def split_dataset(self, mode: str, save: False, **kwargs):
+
+        if mode == "class":
+
+            if 'forget' in kwargs.keys():
+                print(kwargs['forget'])
+
+                if isinstance(kwargs['forget'], list):
+                    print('is list')
+
+                    if isinstance(kwargs['forget'][0], str):
+                        print('is string list')
+
+                        c = []
+                        for i, class_name in enumerate(self.classes):
+                            if class_name in kwargs['forget']:
+                                c.append(i)
+                        
+                        c = np.array(c)
+
+                    elif isinstance(kwargs['forget'][0], int):
+                        print('is int list')
+
+                        if max(kwargs['forget']) < self.n_classes:                        
+                            c = np.array(kwargs['forget'])
+
+                        else:
+                            print(f"\033[31mClass index out of range\r\n Please insert a valid class index!\033[0m")
+                            return None
+
+                    else:
+                        print(f"\033[31mClass format not recognized\r\n Please insert a valid class value!\033[0m")
+                        return None
+                
+                    print(f'class is {c}')
+                    trainf_mask = np.isin(np.array(self.dataset_splits["Train"].targets), c)
+                    testf_mask = np.isin(np.array(self.dataset_splits["Test"].targets), c)
+                    
+                elif isinstance(kwargs['forget'], str):
+                    print('is string')
+
+                    for i, class_name in enumerate(self.classes):
+                        if kwargs['forget'] == class_name:
+                            c = i
+                            break
+
+                    trainf_mask = np.array(self.dataset_splits["Train"].targets) == c
+                    testf_mask = np.array(self.dataset_splits["Test"].targets) == c
+
+                elif isinstance(kwargs['forget'], int):
+                    print('is int')
+                    if kwargs['forget'] < self.n_classes:
+                        trainf_mask = np.array(self.dataset_splits["Train"].targets) == kwargs['forget']
+                        testf_mask = np.array(self.dataset_splits["Test"].targets) == kwargs['forget']
+                    else:
+                        print(f"\033[31mClass index outside of available range\r\n Please insert a valid class index!\033[0m")
+                        return None
+
+                else:
+                    print(f"\033[31mInvalid class\r\n Please insert a valid class or class index!\033[0m")
+                    return None
+
+                train_idx = np.array(range(len(self.dataset_splits["Train"])))
+                test_idx = np.array(range(len(self.dataset_splits["Test"])))
+
+                train_f_idx = train_idx[trainf_mask]
+                train_r_idx = train_idx[~trainf_mask]
+                test_f_idx = test_idx[testf_mask]
+                test_r_idx = test_idx[~testf_mask]
+
+                train_retain = Subset(self.dataset_splits["Train"], train_r_idx)
+                train_forget = Subset(self.dataset_splits["Train"], train_f_idx)
+
+                test_retain = Subset(self.dataset_splits["Test"], test_r_idx)
+                test_forget = Subset(self.dataset_splits["Test"], test_f_idx)
+
+                new_splits =  {"Train_retain": train_retain, "Train_forget": train_forget, "Test_retain": test_retain, "Test_forget": test_forget}
+
+                self.dataset_splits.update(new_splits)
+
+                if save:
+                    self.save_dataset()
+
+            return self.dataset_splits
+
+        elif mode == "random":
+            return None
+        else:
+            return None
+
